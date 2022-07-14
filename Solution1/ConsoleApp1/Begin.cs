2823bd7cde761c00379912b87d05bed7bb4c4c1b
artem <barishartem@gmail.com>
2022-07-14 13:40:00 +0300

    New task

---
+++ Solution1/ConsoleApp1/Begin.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1;
+
+public class Begin
+{
+    
+    public void Begin1()
+    {
+        var a = int.Parse(Console.ReadLine());
+        Console.WriteLine(4*a);
+    }
+}
---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+Begin a = new Begin();
+a.Begin1();
