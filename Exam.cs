8ba8341f9c3f408805e46fff5b795934c0a08082
artem <barishartem@gmail.com>
2022-07-21 14:42:53 +0300

    Changed class Exam

--- Exam.cs
+++ Exam.cs
@@ -1,13 +1,21 @@
 ﻿using _Experiment_;
 namespace Experiment_;
 
-class Exam: Experiment
+class Exam : Experiment
 {
-    protected int passingScore;
+    protected readonly int passingScore;
 
     protected Exam(string nameOfLector, string discipline, int evalua, int passingScore) : base(nameOfLector,
         discipline, evalua)
     {
         this.passingScore = passingScore;
+    }
+
+    static void PassedOrNot(ref Exam exam)
+    {
+        if (exam.evalua >= exam.passingScore)
+            Console.WriteLine("You are Passed");
+        else 
+            Console.WriteLine("You are going to repassed");
     }
 }
