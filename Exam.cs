ac68fc164876cb9646fd75eb839ff9f12486159f
artem <barishartem@gmail.com>
2022-07-21 14:31:16 +0300

    New class Exam

---
+++ Exam.cs
@@ -0,0 +1,13 @@
+﻿using _Experiment_;
+namespace Experiment_;
+
+class Exam: Experiment
+{
+    protected int passingScore;
+
+    protected Exam(string nameOfLector, string discipline, int evalua, int passingScore) : base(nameOfLector,
+        discipline, evalua)
+    {
+        this.passingScore = passingScore;
+    }
+}
