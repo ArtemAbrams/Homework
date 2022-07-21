c9c13d35296d73f8f829a8324db3bcb534a90d13
artem <barishartem@gmail.com>
2022-07-21 12:07:18 +0300

    Inharitance

---
+++ Task/Human.cs
@@ -0,0 +1,21 @@
+﻿namespace Hum
+{
+   class Human
+    {
+        protected  string name;
+        protected  string surname;
+        protected  int age;
+
+        protected internal Human()
+        {
+            
+        }
+        protected internal Human(string name, string surname, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.surname = surname ?? "Undefine";
+            Console.WriteLine("The Human class");
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using Doct;
+using Hum;
+/*string surname = null;
+Doctor doctor = new Doctor("Ton", surname, 13, "56784");
+doctor.Print();*/
+Human obj = new Doctor();
+if (obj is Human human)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+((Doctor)obj).Print();
