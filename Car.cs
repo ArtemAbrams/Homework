16ba0a074807180dc43ad0c5a5ded0f05dac7b1c
artem <barishartem@gmail.com>
2022-07-26 14:20:58 +0300

    The new file - Car

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿namespace Experiment;
+
+class Car: Transport
+{
+    internal string name;
+    internal string mark;
+    internal override string code
+    {
+        get
+        {
+            return base.code;
+        }
+        set
+        {
+            Console.WriteLine("The value");
+            base.code = value;
+        }
+    }
+
+    internal Car (int price, string mark, string name) : base(price)
+    {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("The car " + name + "is driving");
+    }
+}
