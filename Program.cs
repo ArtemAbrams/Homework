5b5bd0106cf0ac4fcf86ef688cc882910b308507
artem <barishartem@gmail.com>
2022-07-29 15:31:31 +0300

    Exception

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
 ﻿using Experiment;
-string id=Console.ReadLine(), name=Console.ReadLine(), phoneNumber=Console.ReadLine();
-string nameOfLector=Console.ReadLine(), discipline=Console.ReadLine();
-int evalua=int.Parse(Console.ReadLine()), numberOfTasks=int.Parse(Console.ReadLine());
-bool typeOfQuestion=bool.Parse(Console.ReadLine());
-Test test = new Test(nameOfLector, discipline, evalua, numberOfTasks, typeOfQuestion);
-Person<Test, string> person = new Person<Test, string>(test, name, id, phoneNumber); 
-person.Print();
