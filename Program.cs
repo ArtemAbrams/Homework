8365aeefda998ed8d061b1df49c0b5829ac0b561
artem <barishartem@gmail.com>
2022-07-29 15:37:21 +0300

    Modified file Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿using Experiment;
+
+try
+{
+    int x = 0;
+    int y = 0;
+    Console.WriteLine(x / y);
+}
+catch 
+{
+    Console.WriteLine("The mistake ");
+}
+finally
+{
+    Console.WriteLine("The try-catch-finally block is end");
+}
+Console.WriteLine("The program is finish");
