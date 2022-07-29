a42b0c318ac2d2563cc21a1edb8de59f52778096
artem <barishartem@gmail.com>
2022-07-29 17:59:43 +0300

    Modified file program and ExceptopnClass

--- ExceptionClass.cs
+++ ExceptionClass.cs
@@ -4,29 +4,12 @@ public class ExceptionClassAlcohol
 {
     public int age;
 
-    public string name
-    {
-        get
-        {
-            return name;
-        }
-        set
-        {
-            try
-            {
-                name = value;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
+    public string name { get; set; }
     public int Age
     {
         set
         {
-            if (Age < 18)
+            if (value < 18)
             {
                 throw new Exception("The age smaller than 18");
             }
@@ -40,33 +23,7 @@ public class ExceptionClassAlcohol
             return age;
         }
     }
-
     public ExceptionClassAlcohol()
     {
-        Console.WriteLine("Enter the name");
-        string name = Console.ReadLine();
-        Console.WriteLine("Enter the age");
-        int age=int.Parse(Console.ReadLine());
-        while (true)
-        {
-            try
-            {
-                this.name = name;
-                if (age < 18)
-                {
-                    throw new Exception("The agg smaller than 18");
-                }
-                else
-                {
-                    this.age = age;
-                }
-
-                break;
-            }
-            catch (Exception e)
-            {
-                 Console.WriteLine(e.Message);
-            }
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,12 @@
 ﻿using Experiment;
 
-void Numeric(int x)
-{
-    if (x >= 0)
-    {
-        throw new Exception("The positive numeric");
-    }
-}
+ExceptionClassAlcohol ex = new ExceptionClassAlcohol();
 try
-{   
-    int x = int.Parse(Console.ReadLine());
-    Numeric(x);
-   /* x = int.Parse(Console.ReadLine());
-    if (x >= 0)
-    {
-        throw new Exception("The positive numeric");
-    }*/
-    
-}
-catch (Exception ex) 
 {
-    Console.WriteLine(ex.Message);
-    Console.WriteLine(ex.StackTrace);
-    Console.WriteLine(ex.TargetSite);
+    ex.Age = 15;
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
 }
 Console.WriteLine("The program is finish");
