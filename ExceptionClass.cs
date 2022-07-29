3b917c667ff21a15502b3390429436c08ac01766
artem <barishartem@gmail.com>
2022-07-29 18:16:33 +0300

    Modified file

--- ExceptionClass.cs
+++ ExceptionClass.cs
@@ -25,5 +25,21 @@ public class ExceptionClassAlcohol
     }
     public ExceptionClassAlcohol()
     {
+        string name;
+        long name_1;
+        while (true)
+        {
+            try
+            {
+                name = Console.ReadLine();
+                this.name = name;
+                name_1 = Convert.ToInt32(name);
+            }
+            catch (Exception ex)
+            {
+                break;
+            }
+        }
+        Console.WriteLine("Try-catch-finally bloc is finished");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
 ﻿using Experiment;
 
 ExceptionClassAlcohol ex = new ExceptionClassAlcohol();
-try
-{
-    ex.Age = 15;
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
 Console.WriteLine("The program is finish");
